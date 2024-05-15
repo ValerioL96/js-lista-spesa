42e1364671cbb03f220c81fde69b3bdac7e9ac00
ValerioL96 <v.lembo1396@gmail.com>
2024-05-15 18:05:07 +0200

    Added condition with while in JavaScript

--- js/script.js
+++ js/script.js
@@ -17,3 +17,32 @@
 //2) creo un contenitore ul 
 
     const ulElement = document.querySelector('ul');
+
+//3) ho una condizione finchè l'elemnto è contenuto nella mia lista viene stampato nel mio contenitore:
+     //-pongo un indice = 0
+     let i = 0;
+
+     //-creo un while con la condizione che il mio indice sia minore della lunghezza della mia lista spesa 
+     while(i< shoppingList.length){
+
+        //-al suo interno mi creo degli elementi 'li' ;
+        const element = document.createElement('li');
+
+        //-aggiungo questo elemnti alla mia lista spesa;
+
+        element.append(shoppingList[i]);
+
+        //-aggiungo un nodo come ultimo figlio ad un nodo genitore che in questo caso è ul;
+
+        ulElement.appendChild(element);
+
+        //-indice aumenta ad ogni elemento;
+
+        i++;
+
+     }
+   
+   
+   
+   
+ 
