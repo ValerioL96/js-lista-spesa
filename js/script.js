6707a067a9a6f7b904ccc1d77c4c334dd1afa494
ValerioL96 <v.lembo1396@gmail.com>
2024-05-15 16:48:01 +0200

    Added in Javascript shoppinglist

---
+++ js/script.js
@@ -0,0 +1,14 @@
+//Consegna
+
+//Data una lista della spesa, stampare sulla pagina (anche brutalmente, basta che si vedano) gli elementi della lista individualmente con un ciclo while.
+
+//1) creo una lista spesa con un array 
+
+   const shopList = [
+     'Pane',
+     'Banana',
+     'Latte',
+     'Carote',
+     'Pollo',
+     'Acqua'
+    ];
